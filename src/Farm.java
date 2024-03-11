a74c04c2c568f17263e6b969a9cc5dc817c75f3f
Stanislav <stanislav.petr06@gmail.com>
2024-03-11 08:08:48 +0100

    added new three metods and Lists for Animal and Flower and added new class Sunflower

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,12 @@
+import java.util.List;
+
 public class Farm {
+    List<Animal> zvirata;
+    List<Flower> rostlina;
+    public void zasadit(){}
+    public void zavlazit(){}
+    public void sklizet(){}
+
 
 
 
